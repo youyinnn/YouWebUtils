f3b6788f7ab3ffa768a18f302e743c25a64e09a6
youyinnn <454947049@qq.com>
2018-05-27 00:10:15 +0800

    IMPMODU CGREADME RELEASE
    IMPMODU:
    1.增加内置属性的单个获取方法
    
    CGREADME:*
    
    RELEASE:1.8.4

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## YouWebUtils
-[![Travis](https://img.shields.io/badge/version-1.8.1-green.svg)]()
+[![Travis](https://img.shields.io/badge/version-1.8.4-green.svg)]()
 [![Travis](https://img.shields.io/badge/servletapi-3.1.0-brightgreen.svg)]()
 [![Travis](https://img.shields.io/badge/log4j2-2.9.0-brightgreen.svg)]()
 [![Travis](https://img.shields.io/badge/cos-26Dec2008-brightgreen.svg)]()
@@ -7,38 +7,54 @@
 [![Travis](https://img.shields.io/badge/javajwt-3.2.0-brightgreen.svg)]()
 [![Travis](https://img.shields.io/badge/dom4j-1.6.1-brightgreen.svg)]()
 [![Travis](https://img.shields.io/badge/jaxen-1.1.6-brightgreen.svg)]()
-##### 这是一个“自用”的WEb开发工具包
 
-- - -
+**这是一个“自用”的Web开发工具包**
 
-### TODO:
+#### 介绍
 
-- 待添加
+- 分4个梯队
+    - first：主要放和web组件相关的基层类，如BaseHttpFilter
+    - second：主要放web开发常用工具类，如：
+        - Json处理
+            - json模板池
+            - json对象与json串转换
+            - json以key寻value
+            - json串处理
+        - Jwt处理
+            - 签发token
+            - 验证token
+        - Properties处理
+            - 常用系统prop的快速获取
+            - pid获取
+            - 快速load一个properties文件
+    - third：主要放项目开发常用工具类，如：
+        - Class处理
+            - 提供扫描类等方法
+        - 数据库处理
+            - 库处理：从连接中获取所连接的库名/从库中获取所有表名
+            - 表处理：表存在/从表中获取所有列名
+            - 类名处理：转换类名为符合阿里巴巴手册对应的表名格式
+            - sql脚本执行
+        - Dom4j封装处理
+            - 快速xml文件读取
+        - Log4j2封装处理
+            - 分主次地融合多个具有log4j2配置文件格式的xml文件（实现嵌入log4j2配置）
+        - 反射工具类
+        - sql脚本执行工具类
+        - 集合框架工具类
+    - fourth：主要放前三个梯队所需辅助类
 
-- - -
-
-### 使用
+#### 使用
 
 maven:
 ```xml
         <dependency>
             <groupId>com.github.youyinnn</groupId>
             <artifactId>you-web-utils</artifactId>
-            <version>1.8.1</version>
+            <version>1.8.4</version>
         </dependency>
 ```
 
-- - -
+#### TODO:
 
-### 索引
-- [介绍](#介绍)
-- [架构](#架构)
-  - [依赖](#依赖)
-  - [架构、惯例](#架构惯例)
-- [示例](#示例)
-
-- - -
-
-<span id="介绍"/>
-
-#### 介绍
+- 待添加
--- src/main/java/com/github/youyinnn/youwebutils/second/PropertiesHelper.java
+++ src/main/java/com/github/youyinnn/youwebutils/second/PropertiesHelper.java
@@ -12,6 +12,8 @@ import java.util.Properties;
  */
 public class PropertiesHelper {
 
+    private static Properties props= System.getProperties();
+
     private PropertiesHelper(){}
 
     public static void load(String proFilePath, Properties properties) {
@@ -23,42 +25,81 @@ public class PropertiesHelper {
     }
 
     public static void printSystemProperties(){
-        Properties props= System.getProperties();
         System.out.println("===============================================================================================");
-        System.out.println("Java的运行环境版本："+props.getProperty("java.version"));
-        System.out.println("Java的类路径："+props.getProperty("java.class.path"));
-        System.out.println("加载库时搜索的路径列表："+props.getProperty("java.library.path"));
-        System.out.println("默认的临时文件路径："+props.getProperty("java.io.tmpdir"));
-        System.out.println("操作系统的名称："+props.getProperty("os.name"));
-        System.out.println("操作系统的构架："+props.getProperty("os.arch"));
-        System.out.println("操作系统的版本："+props.getProperty("os.version"));
-        System.out.println("文件分隔符："+props.getProperty("file.separator"));
-        System.out.println("路径分隔符："+props.getProperty("path.separator"));
-        System.out.println("行分隔符："+props.getProperty("line.separator"));
-        System.out.println("用户的账户名称："+props.getProperty("user.name"));
-        System.out.println("用户的主目录："+props.getProperty("user.home"));
-        System.out.println("用户的当前工作目录："+props.getProperty("user.dir"));
+        System.out.println("Java的运行环境版本：" + getJavaVersion());
+        System.out.println("Java的类路径：" + getJavaClassPath());
+        System.out.println("加载库时搜索的路径列表：" + getJavaLibraryPath());
+        System.out.println("默认的临时文件路径：" + getIoTempDir());
+        System.out.println("操作系统的名称：" + getOsName());
+        System.out.println("操作系统的构架：" + getOsArch());
+        System.out.println("操作系统的版本：" + getOsVersion());
+        System.out.println("文件分隔符：" + getFileSeparator());
+        System.out.println("路径分隔符：" + getPathSeparator());
+        System.out.println("行分隔符：" + getLineSeparator());
+        System.out.println("用户的账户名称：" + getUserName());
+        System.out.println("用户的主目录：" + getUserHome());
+        System.out.println("用户的当前工作目录：" + getUserDir());
         System.out.println("===============================================================================================");
     }
 
     public static String getSystemPropertiesJson() {
-        Properties props= System.getProperties();
-        HashMap<String , String> propMap = new HashMap<>();
-        propMap.put("java.version", props.getProperty("java.version"));
-        propMap.put("java.io.tmpdir", props.getProperty("java.io.tmpdir"));
-        propMap.put("os.name", props.getProperty("os.name"));
-        propMap.put("os.arch", props.getProperty("os.arch"));
-        propMap.put("os.version", props.getProperty("os.version"));
-        propMap.put("file.separator", props.getProperty("file.separator"));
-        propMap.put("path.separator", props.getProperty("path.separator"));
-        propMap.put("line.separator", props.getProperty("line.separator"));
-        propMap.put("user.name", props.getProperty("user.name"));
-        propMap.put("user.home", props.getProperty("user.home"));
-        propMap.put("user.dir", props.getProperty("user.dir"));
+        HashMap<String , String> propMap = new HashMap<>(11);
+        propMap.put("java.version", getJavaVersion());
+        propMap.put("java.io.tmpdir", getIoTempDir());
+        propMap.put("os.name", getOsName());
+        propMap.put("os.arch", getOsArch());
+        propMap.put("os.version", getOsVersion());
+        propMap.put("file.separator", getFileSeparator());
+        propMap.put("path.separator", getPathSeparator());
+        propMap.put("line.separator", getLineSeparator());
+        propMap.put("user.name", getUserName());
+        propMap.put("user.home", getUserHome());
+        propMap.put("user.dir", getUserDir());
         return JSON.toJSONString(propMap);
     }
 
     public static String getPID() {
         return ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
     }
+
+    public static String getJavaVersion() {
+        return props.getProperty("java.version");
+    }
+    public static String getIoTempDir() {
+        return props.getProperty("java.io.tmpdir");
+    }
+    public static String getOsName() {
+        return props.getProperty("os.home");
+    }
+    public static String getOsArch() {
+        return props.getProperty("os.arch");
+    }
+    public static String getOsVersion() {
+        return props.getProperty("os.version");
+    }
+    public static String getFileSeparator() {
+        return props.getProperty("file.separator");
+    }
+    public static String getPathSeparator() {
+        return props.getProperty("path.separator");
+    }
+    public static String getLineSeparator() {
+        return props.getProperty("line.separator");
+    }
+    public static String getUserName() {
+        return props.getProperty("user.name");
+    }
+    public static String getUserDir() {
+        return props.getProperty("user.dir");
+    }
+    public static String getUserHome() {
+        return props.getProperty("user.home");
+    }
+    public static String getJavaClassPath() {
+        return props.getProperty("java.class.path");
+    }
+    public static String getJavaLibraryPath() {
+        return props.getProperty("java.library.path");
+    }
+
 }
